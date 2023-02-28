e94a930c49a7bf6927d40439da95ddc23585d9c8
phamchien789vn <phamchienvn91@gmail.com>
2023-02-28 17:38:23 +0700

    Update function create Account

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,7 +1,7 @@
 
-const Role = require('../models/User');
+const User = require('../models/User');
 
-// GET: Home Page
+// Start: GET: Home Page
 const dashboardView = async (req, res) => {
     try {
         const title = 'Home';
@@ -9,18 +9,120 @@ const dashboardView = async (req, res) => {
 
         // If the user has a role, fetch the role data using the populate() method
         if (user.role) {
-            const role = await Role.Role.findById(user.role);
+            const role = await User.Role.findById(user.role);
             user.role = role;
         }
 
         res.render('Admin/home', { user, title });
     } catch (error) {
         console.error(error);
-        res.status(500).send('Internal Server Error');
+        res.redirect('/');
+        // res.status(500).send('Internal Server Error');
     }
 };
-// GET: Home Page
+// End: GET: Home Page
+
+// Start: GET: Create Account Page
+const formAccountView = async (req, res) => {
+    try {
+        const title = 'Create Account';
+        const user = req.user;
+
+        // If the user has a role, fetch the role data using the populate() method
+        if (user.role) {
+            const role = await User.Role.findById(user.role);
+            user.role = role;
+        }
+
+        let allRoles = [];
+
+        User.Role.find().exec((err, datas) => {
+            if (err) {
+                console.error(err);
+            } else {
+                allRoles = datas; // lưu kết quả vào biến ngoài allRoles
+                res.render('Admin/formAccount', { user, title, allRoles })
+            }
+        });
+
+    } catch (error) {
+        console.error(error);
+        // res.status(500).send('Internal Server Error');
+        res.redirect('/');
+    }
+}
+// End: GET: Create Account Page
+
+// Start: POST: Create Account
+const submitFormAccount = (req, res, next) => {
+    User.User.create(req.body);
+    console.log(req.body);
+    res.redirect('/listAccounts');
+};
+// End: POST: Create Account
+
+// Start: GET: List Account Page
+const listAccountsView = async (req, res, next) => {
+    try {
+        const title = 'List Accounts';
+        const user = req.user;
+
+        const data = [];
+
+        // If the user has a role, fetch the role data using the populate() method
+        if (user.role) {
+            const role = await User.Role.findById(user.role);
+            user.role = role;
+        }
+
+        User.Role.find().exec((err, datas) => {
+            if (err) {
+                console.error(err);
+            } else {
+                const allRoles = datas; // lưu kết quả vào biến allRoles
+
+                allRoles.forEach(role => {
+                    if (role.name === 'Staff') {
+                        const role_ID = role._id;
+
+                        console.log(role.name);
+                        console.log(role_ID);
+
+                        User.User.find({ role: role_ID })
+                            .then(staffs => {
+                                console.log(typeof staffs)
+
+                                var staffss = staffs;
+
+                                var resultArray = Object.keys(staffss).map(function (personNamedIndex) {
+                                    let person = staffss[personNamedIndex];
+                                    // do something with person
+                                    return person;
+                                });
+
+                                res.render('Admin/listAccounts', { user, title, resultArray });
+
+                            })
+                            .catch(error => {
+                                console.log(error);
+                            });
+                    }
+                });
+            }
+        });
+
+
+
+
+    } catch (error) {
+        console.error(error);
+        res.redirect('/');
+        // res.status(500).send('Internal Server Error');
+    }
+}
+// Start: GET: List Account Page
+
 
 module.exports = {
-    dashboardView,
+    dashboardView, formAccountView, submitFormAccount, listAccountsView
 };
--- routes/route.js
+++ routes/route.js
@@ -5,7 +5,8 @@ const { loginView, loginUser } = require('../controllers/loginController');
 const { protectRoute } = require("../auth/protect");
 const { logoutUser } = require('../controllers/loginController');
 
-const { dashboardView } = require('../controllers/adminController');
+const { dashboardView, formAccountView, submitFormAccount, listAccountsView } = require('../controllers/adminController');
+const { Role } = require('../models/User');
 
 // Define Routes
 const router = express.Router();
@@ -15,4 +16,43 @@ router.post("/login", loginUser);
 router.get("/", protectRoute, dashboardView);
 
 router.get('/logout', logoutUser);
+
+// Start: Route Admin site
+router.get('/formAccount', formAccountView);
+router.post('/submitFormAccount', submitFormAccount);
+router.get('/listAccounts', listAccountsView);
+
+// End: Route Admin site
+
+
+router.get('/save', async function (req, res, next) {
+    // Create role Admin if not exists
+    await Role.findOneAndUpdate(
+        { name: 'Admin' },  // Query to find existing document
+        { name: 'Admin' },          // Data to update or insert new document
+        { upsert: true },     // Upsert option set to true to create if does not exist
+    );
+
+    // Create Role Quality Assurance Manager if not exists
+    await Role.findOneAndUpdate(
+        { name: 'QA Manager' },  // Query to find existing document
+        { name: 'QA Manager' },          // Data to update or insert new document
+        { upsert: true },     // Upsert option set to true to create if does not exist
+    );
+
+    // Create Role Quality Assurance Coordinator if not exists
+    await Role.findOneAndUpdate(
+        { name: 'QA Coordinator' },  // Query to find existing document
+        { name: 'QA Coordinator' },          // Data to update or insert new document
+        { upsert: true },     // Upsert option set to true to create if does not exist
+    );
+
+    // Create Role Staff if not exists
+    await Role.findOneAndUpdate(
+        { name: 'Staff' },  // Query to find existing document
+        { name: 'Staff' },          // Data to update or insert new document
+        { upsert: true },     // Upsert option set to true to create if does not exist
+    );
+});
+
 module.exports = router;
