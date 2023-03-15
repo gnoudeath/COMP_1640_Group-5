1b6504d1ee7e4c916be33ee347791a5186d6d79b
Nguyen Dung <dungnagch190701@fpt.edu.vn>
2023-03-15 20:05:20 +0700

    Modify filter and UI

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -4,6 +4,7 @@ const { Idea, Category } = require('../models/Idea');
 const dateTimeFormat = 'DD/MM/YYYY HH:mm';
 const moment = require('moment-timezone');
 const timezone = 'Asia/Ho_Chi_Minh';
+const mongoose = require("mongoose");
 
 // For View 
 const loginView = (req, res) => {
@@ -156,10 +157,171 @@ const dashboardView = async (req, res) => {
   }
 };
 // End: GET: Home Page
+const dashboardView2 = async (req, res) => {
+
+  try {
+    const categories = await Category.find({});
+    const title = 'Home';
+    const messages = req.flash('error');
+    const user = req.user;
+    let perPage = 6;
+    let page = req.query.page || 1;
+    const sortBy = req.query.sortBy || 'all-ideas'
+    const category = req.query.category || 'all'
+    
+
+    console.log(category)
+    let sortOptions;
+    if (sortBy === 'all-ideas') {
+      sortOptions = { commentCount: -1,createdDate: -1 };
+    } else if (sortBy === 'most-viewed') {
+      sortOptions = { viewCount: -1,createdDate: -1 };
+    } else if (sortBy === 'last-ideas') {
+      sortOptions = { createdDate: -1};
+    } else if (sortBy === 'last-comments') {
+      sortOptions = { latestCommentDate: -1,createdDate: -1 };
+    }
+    else {
+      // Default sort option
+      sortOptions = { commentCount: -1 };
+    }
+
+    
+    const query = {};
+
+  // Filter by category
+  let categoryName = 'All Categories';
+  if (category && category !== 'all') {
+    query.category = mongoose.Types.ObjectId(category);
+    const selectedCategory = await Category.findById(category);
+    categoryName = selectedCategory.nameCate;
+  }
+
+    
+
+    const ideas = await Idea.aggregate([
+      {
+        $match: query
+      },
+      
+      // perform a left join between Idea and Comment collections
+      {
+        $lookup: {
+          from: 'comments', // the name of the Comment collection
+          localField: '_id',
+          foreignField: 'idea',
+          as: 'comments'
+        }
+      },
+      // group by Idea id and count the number of comments for each Idea
+      {
+        $group: {
+          _id: '$_id',
+          title: { $first: '$title' },
+          content: { $first: '$content' },
+          category: { $first: '$category' },
+          user: { $first: '$user' },
+          like: { $first: '$like' },
+          dislike: { $first: '$dislike' },
+          createdDate: { $first: '$createdDate' },
+          commentCount: { $sum: { $size: '$comments' } },
+          viewedBy: { $addToSet: '$viewedBy' },
+          viewCount: { $sum: { $cond: [{ $isArray: '$viewedBy' }, { $size: '$viewedBy' }, 0] } },
+          latestCommentDate: { $max: '$comments.created_at' }
+
+        }
+      },
+      // populate user and category fields
+      {
+        $lookup: {
+          from: 'users', // the name of the User collection
+          localField: 'user',
+          foreignField: '_id',
+          as: 'user'
+        }
+      },
+      {
+        $lookup: {
+          from: 'categories', // the name of the Category collection
+          localField: 'category',
+          foreignField: '_id',
+          as: 'category'
+        }
+      },
+      
+      // project only the necessary fields
+      {
+        $project: {
+          title: 1,
+          content: 1,
+          user: { $arrayElemAt: ['$user.username', 0] },
+          category: { $arrayElemAt: ['$category.name', 0] },
+          createdDate: 1,
+          commentCount: 1,
+          like: 1,
+          dislike: 1,
+          viewCount: 1,
+          latestCommentDate: 1
+        }
+      },
+      // sort by descending comment count
+      {
+        $sort: 
+        sortOptions
+        
+      },
+      // skip and limit based on pagination
+      {
+        $skip: (perPage * page) - perPage
+      },
+      {
+        $limit: perPage
+      }
+    ]).exec();;
+    
+    const formattedList = ideas.map(item => {
+      return {
+          createdDate: moment(item.createdDate).tz(timezone).format(dateTimeFormat),
+      };
+  });
+
+    const count = await Idea.countDocuments(query);
+    if (user.role) {
+      const role = await User.Role.findById(user.role);
+      user.role = role;
+      // const formattedList = ideas.map(item => {
+      //   return {
+      //     createdDate: moment(item.createdDate).tz(timezone).format(dateTimeFormat),
+      //   };
+      // });
+      res.render('home', {
+        user,
+        ideas,
+        current: page,
+        pages: Math.ceil(count / perPage),
+        title,
+        messages,
+        formattedList,
+        sortBy,
+        category,
+        categories,
+        categoryName
+      });
+    } else {
+      res.redirect('/login');
+    }
+  }
+  catch (error) {
+    console.error(error);
+    res.redirect('/');
+    // res.status(500).send('Internal Server Error');
+  }
+};
 
 module.exports = {
   logoutUser,
   loginView,
   loginUser,
-  dashboardView
+  dashboardView,
+  dashboardView2
 };
